646e68f7d8a167f6be21a9edcdab36fdbdcd8a34
yijiejiang <yijiejiang@tencent.com>
2016-09-11 16:00:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+import Vue from 'vue'
+import "./assets/less/app1.less"
+const _lodash = require('lodash');
+
+
+export default {
+    components: {
+    },
+}
+</script>
